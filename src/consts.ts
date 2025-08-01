82bebac69056a6c620d02704749d1785182a7173
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2025-08-01 14:03:04 -0500

    feat(app): fix bg texture, accents + btn styling, containers

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'JTBI Imaginative'
+export const SITE_TITLE = 'JTBI Imaginative LLC'
 export const SITE_DESCRIPTION =
   'JTB Imaginative LLC crafts brand identities for new ventures and small-to-medium businesses that are designed to capture attention, support growth, and scale gracefully.'
 export const SITE_URL = 'http://localhost:4321' // TODO: change to production url
