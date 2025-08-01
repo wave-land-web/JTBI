8e562dcef0ebfa92ce2f36092f46bb7c5f0b726e
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2025-07-31 20:08:12 -0500

    feat(app): first commit, styles, layout, branded elements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,39 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // TODO: replace with prod site URL
+  site: 'http://localhost:4321',
+  adapter: netlify({
+    imageCDN: false,
+    cacheOnDemandPages: true,
+  }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  image: {
+    layout: 'constrained',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
+  },
+  prefetch: {
+    prefetchAll: true,
+  },
+  experimental: {
+    // SEE: https://docs.astro.build/en/reference/experimental-flags/fonts/#local-font-variants
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Radio Canada',
+        cssVariable: '--font-radio-canada',
+      },
+    ],
+  },
+})
